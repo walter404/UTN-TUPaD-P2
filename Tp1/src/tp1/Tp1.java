f56bae9b0cf14dc3c62e834ebbf578e238393da1
walter404 <walterame123@gmail.com>
2025-08-18 16:02:22 -0300

    primer tp

--- Tp1/src/tp1/Tp1.java
+++ Tp1/src/tp1/Tp1.java
@@ -1,12 +1,73 @@
 
 package tp1;
 
+import java.util.Scanner;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
+
 
 public class Tp1 {
-
+    final static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        /* Ejercicio 3
+        String nombre = "Walter";
+        int edad = 25;
+        double altura = 1.85;
+        boolean estudiante = true;
+        
+        System.out.println(" Me llamo " +nombre + " tengo " + edad + " años " + "mido " + altura + " mtrs Estudiante: " + estudiante);
+        */
+        
+        /* Ejercicio 4
+        System.out.print("Ingrese su nombre: ");
+        String nombre = input.nextLine();
+        
+        System.out.print("Ingrese su edad: ");
+        int edad = Integer.parseInt(input.nextLine());
+        
+        System.out.println("Su nombre es " + nombre + ", su edad es " + edad + " años");
+        */
+        
+        /* Ejercicio 5
+        System.out.print("Ingrese el primer numero: ");
+        int numero1 = Integer.parseInt(input.nextLine());
+        
+        System.out.print("Ingrese el segundo numero: ");
+        int numero2 = Integer.parseInt(input.nextLine());
+        
+        System.out.println("Suma: " + (numero1 + numero2));
+        System.out.println("Resta: " + (numero1 - numero2));
+        System.out.println("Division: " + (numero1 / numero2));
+        System.out.println("Multiplicacion: " + (numero1 * numero2));
+        */
+        
+        /* Ejercicio 6 
+        String nombre = "Juan Perez";
+        int edad = 30;
+        String direccion = "Calle Falsa 123";
+        
+        System.err.println("Nombre: " + nombre + "\nEdad: " + edad + "\nDireccion: " + direccion);
+        */
+        
+        /* Ejercicio 8
+        System.out.println("Ingrese un numero: ");
+        int numero1 = Integer.parseInt(input.nextLine());
+        
+        System.out.println("Ingrese uun segundo numero: ");
+        int numero2 = Integer.parseInt(input.nextLine());
+        
+        System.out.println("Divisiones de numero enteros: " + (numero1 / numero2));
+        System.out.println("Divisiones de numero con doble: " + ((double)numero1 / 0));
+        */
+        
+        /* Ejercicio 9
+        System.out.println("Ingresa tu nombre: ");
+        // String nombre = input.nextInt(); // ERROR 
+        String nombre = input.nextLine(); // CORREGIDO
+        
+        System.out.println("Hola, " + nombre);
+        */
+        
     }
     
 }
